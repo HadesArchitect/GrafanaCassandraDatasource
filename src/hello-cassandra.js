f6dfb5d7be9cd4fc64a494b8d40bf32c338a4111
Alex Slavyansky <alex.slavyansky@gmail.com>
2020-01-10 00:20:10 +0300

    Added Cassandra Driver

---
+++ src/hello-cassandra.js
@@ -0,0 +1,11 @@
+console.log("Hello Cassandra!");
+
+const cassandra = require('cassandra-driver');
+const client = new cassandra.Client({ contactPoints: ['localhost:9042'], localDataCenter: 'dc1'});
+
+const query = 'SELECT * FROM videodb.videos';
+client.execute(query)
+  .then(result => {
+    const row = result.first();
+    console.log('User lives in %s, %s', row.videoname, row.description); 
+  });
