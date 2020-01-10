d99ef2a95d0ba27b78f9d4c61232fa349eb04e20
Alex Slavyansky <alex.slavyansky@gmail.com>
2020-01-11 02:17:54 +0300

    Added new plugin in grafana-data/plugins/. New plugin successfuly defined in Grafana

---
+++ grafana-data/plugins/grafana-cassandra-datasource/src/datasource.js
@@ -0,0 +1,70 @@
+import _ from "lodash";
+
+export class GenericDatasource {
+
+  constructor(instanceSettings, $q, backendSrv, templateSrv) {
+    this.type = instanceSettings.type;
+    this.url = instanceSettings.url;
+    this.name = instanceSettings.name;
+    this.q = $q;
+    this.backendSrv = backendSrv;
+    this.templateSrv = templateSrv;
+    this.withCredentials = instanceSettings.withCredentials;
+    this.headers = {'Content-Type': 'application/json'};
+    if (typeof instanceSettings.basicAuth === 'string' && instanceSettings.basicAuth.length > 0) {
+      this.headers['Authorization'] = instanceSettings.basicAuth;
+    }
+  }
+  
+  // Json version
+
+  testDatasource() {
+    return this.doRequest({
+      url: this.url + '/',
+      method: 'GET',
+    }).then(response => {
+      if (response.status === 200) {        
+        return { status: "success", message: "Data source is working", title: "Success" };        
+      }
+    });
+  }
+
+  // console.log ('Status: %, message: %s, title: %s', status, message, title);
+  
+  // MySQL version
+  /*
+  testDatasource() {
+    return this.backendSrv
+      .datasourceRequest({
+        url: '/api/tsdb/query',
+        method: 'POST',
+        data: {
+          from: '5m',
+          to: 'now',
+          queries: [
+            {
+              refId: 'A',
+              intervalMs: 1,
+              maxDataPoints: 1,
+              datasourceId: this.id,
+              rawSql: 'SELECT 1',
+              format: 'table',
+            },
+          ],
+        },
+      })
+      .then((res: any) => {
+        return { status: 'success', message: 'Database Connection OK' };
+      })
+      .catch((err: any) => {
+        console.log(err);
+        if (err.data && err.data.message) {
+          return { status: 'error', message: err.data.message };
+        } else {
+          return { status: 'error', message: err.status };
+        }
+      });
+  }
+  */
+
+}
