096a406507dc9e30189a355b493fdba20d2fdc88
futuarmo <armen114@yandex.ru>
2022-01-24 00:30:42 +0300

    Issue #89: change query editor type button

--- src/QueryEditor.tsx
+++ src/QueryEditor.tsx
@@ -16,6 +16,11 @@ export class QueryEditor extends PureComponent<Props> {
     onChange({ ...query, datasourceId: props.datasource.id });
   }
 
+  onChangeQueryType = () => {
+    const { onChange, query } = this.props;
+    onChange({ ...query, rawQuery:  !query.rawQuery});
+  }
+
 
   onQueryTextChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { onChange, query, onRunQuery } = this.props;
@@ -66,98 +71,104 @@ export class QueryEditor extends PureComponent<Props> {
   };
 
   render() {
+    const options = this.props;
+
     return (
-      
-      <>
-        <InlineFieldRow>
-          <InlineField label="Keyspace" tooltip="Specify keyspace to work with" grow>
-            <Input
-              name="keyspace"
-              value={this.props.query.keyspace || ''}
-              placeholder="keyspace name"
-              onChange={this.onKeyspaceChange}
-              spellCheck={false}
-            />
-          </InlineField>
-        </InlineFieldRow>
-        <InlineFieldRow>
-          <InlineField label="Table" tooltip="Specify table to work with" grow>
-            <Input
-              name="table"
-              value={this.props.query.table || ''}
-              placeholder="table name"
-              onChange={this.onTableChange}
-            />
-          </InlineField>
-        </InlineFieldRow>
-        <InlineFieldRow>
-          <InlineField label="Time Column" tooltip="Specify name of a timestamp column to identify time (created_at, time etc.)" grow>
-            <Input
-              name="time_column"
-              value={this.props.query.columnTime || ''}
-              onChange={this.onTimeColumnChange}
-            />
-          </InlineField>
-        </InlineFieldRow>
-        <InlineFieldRow>
-          <InlineField label="Value Column" tooltip="Specify name of a numeric column to retrieve value (temperature, price etc.)" grow>
-            <Input
-              name="value_column"
-              value={this.props.query.columnValue || ''}
-              onChange={this.onValueColumnChange}
-            />
-          </InlineField>
-        </InlineFieldRow>
-        <InlineFieldRow>
-          <InlineField label="ID Column" tooltip="Specify name of a UUID column to identify the row (id, sensor_id etc.)" grow>
-            <Input
-              name="id_column"
-              value={this.props.query.columnValue || ''}
-              onChange={this.onIDColumnChange}
-            />
-          </InlineField>
-        </InlineFieldRow>
-        <InlineFieldRow>
-          <InlineField label="ID Value" tooltip="Specify UUID value of a column to identify the row (f.e. 123e4567-e89b-12d3-a456-426655440000)" grow>
-            <Input
-              name="value_column"
-              placeholder="123e4567-e89b-12d3-a456-426655440000"
-              value={this.props.query.columnValue || ''}
-              onChange={this.onIDValueChange}
-            />
-          </InlineField>
-        </InlineFieldRow>
-        <InlineFieldRow>
-            <InlineField 
-              label="Allow filtering"
-              tooltip="Allow Filtering can be dangerous practice and we strongly discourage using it"
-            >
-              <Switch value={this.props.query.filtering} onChange={this.onFilteringChange} />
-            </InlineField>
-        </InlineFieldRow>
-        <InlineFieldRow>
-          <Input
-            type="hidden"
-            name="value_column"
-            placeholder="123e4567-e89b-12d3-a456-426655440000"
-            value={this.props.query.columnValue || ''}
-            onChange={this.onIDValueChange}
-          />
-        </InlineFieldRow>
+      <div>
         <Button
-          icon="pen"
-          variant="secondary"
-          aria-label="Toggle editor mode"
-          onClick={() => {
-            console.log("hy")
-          }}
+              icon="pen"
+              variant="secondary"
+              aria-label="Toggle editor mode"
+              onClick={this.onChangeQueryType}
         />
-        <QueryField
-          
-          placeholder={'Enter a Graphite query (run with Shift+Enter)'}
-          portalOrigin="graphite"
-        />
-      </>
+        {
+          options.query.rawQuery &&
+          <QueryField
+            placeholder={'Enter a Graphite query (run with Shift+Enter)'}
+            portalOrigin="graphite"
+          />
+        }
+        {!options.query.rawQuery && 
+          <>
+            <InlineFieldRow>
+              <InlineField label="Keyspace" tooltip="Specify keyspace to work with" grow>
+                <Input
+                  name="keyspace"
+                  value={this.props.query.keyspace || ''}
+                  placeholder="keyspace name"
+                  onChange={this.onKeyspaceChange}
+                  spellCheck={false}
+                />
+              </InlineField>
+            </InlineFieldRow>
+            <InlineFieldRow>
+              <InlineField label="Table" tooltip="Specify table to work with" grow>
+                <Input
+                  name="table"
+                  value={this.props.query.table || ''}
+                  placeholder="table name"
+                  onChange={this.onTableChange}
+                />
+              </InlineField>
+            </InlineFieldRow>
+            <InlineFieldRow>
+              <InlineField label="Time Column" tooltip="Specify name of a timestamp column to identify time (created_at, time etc.)" grow>
+                <Input
+                  name="time_column"
+                  value={this.props.query.columnTime || ''}
+                  onChange={this.onTimeColumnChange}
+                />
+              </InlineField>
+            </InlineFieldRow>
+            <InlineFieldRow>
+              <InlineField label="Value Column" tooltip="Specify name of a numeric column to retrieve value (temperature, price etc.)" grow>
+                <Input
+                  name="value_column"
+                  value={this.props.query.columnValue || ''}
+                  onChange={this.onValueColumnChange}
+                />
+              </InlineField>
+            </InlineFieldRow>
+            <InlineFieldRow>
+              <InlineField label="ID Column" tooltip="Specify name of a UUID column to identify the row (id, sensor_id etc.)" grow>
+                <Input
+                  name="id_column"
+                  value={this.props.query.columnValue || ''}
+                  onChange={this.onIDColumnChange}
+                />
+              </InlineField>
+            </InlineFieldRow>
+            <InlineFieldRow>
+              <InlineField label="ID Value" tooltip="Specify UUID value of a column to identify the row (f.e. 123e4567-e89b-12d3-a456-426655440000)" grow>
+                <Input
+                  name="value_column"
+                  placeholder="123e4567-e89b-12d3-a456-426655440000"
+                  value={this.props.query.columnValue || ''}
+                  onChange={this.onIDValueChange}
+                />
+              </InlineField>
+            </InlineFieldRow>
+            <InlineFieldRow>
+                <InlineField 
+                  label="Allow filtering"
+                  tooltip="Allow Filtering can be dangerous practice and we strongly discourage using it"
+                >
+                  <Switch value={this.props.query.filtering} onChange={this.onFilteringChange} />
+                </InlineField>
+            </InlineFieldRow>
+            <InlineFieldRow>
+              <Input
+                type="hidden"
+                name="value_column"
+                placeholder="123e4567-e89b-12d3-a456-426655440000"
+                value={this.props.query.columnValue || ''}
+                onChange={this.onIDValueChange}
+              />
+            </InlineFieldRow>
+          </>
+        }
+      </div>
+      
     );
   }
 }
